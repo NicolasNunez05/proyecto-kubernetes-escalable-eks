541f5d59bd41363c150a2cf5db75c757cff52e92
eva0555 <nicolasnunezalvarez05@gmail.com>
2026-02-10 18:46:59 -0300

    chore: actualizar backend.tf con nueva cuenta AWS 592451843842
    - Bucket Terraform State: gpuchile-tfstate-592451843842-20260210
    - DynamoDB Locks: gpuchile-terraform-locks
    - Region: us-east-1

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,12 +1,9 @@
-# terraform/backend.tf
 terraform {
   backend "s3" {
-    # 👇 AQUÍ PONES EL NOMBRE EXACTO QUE ACABAS DE CREAR
-    bucket         = "gpuchile-terraform-state-nicolas-2026"
+    bucket         = "gpuchile-tfstate-592451843842-20260210"
     key            = "eks-cluster/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "gpuchile-terraform-locks"
   }
 }
-
